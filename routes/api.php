3f10bf90f627fea5124e8f8272c065bc963f006e
faizendra <faizendra09@gmail.com>
2021-12-18 16:31:34 +0700

    Auth

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,13 @@ use App\Http\Controllers\AuthorController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('book', BookController::class)->except('edit', 'create');
-Route::resource('author', AuthorController::class)->except('edit', 'create');
+//  login register
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// protected
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('book', BookController::class)->except('edit', 'create');
+    Route::resource('author', AuthorController::class)->except('edit', 'create');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
